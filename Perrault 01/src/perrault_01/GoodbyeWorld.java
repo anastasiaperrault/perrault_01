62c96952e5d768f83897bf676aa5f636e8bf373d
anastasiaperrault <anastasiaperrault@192.168.1.135>
2020-01-18 20:40:42 -0500

    First commit for Assignment 1

---
+++ Perrault 01/src/perrault_01/GoodbyeWorld.java
@@ -0,0 +1,11 @@
+package perrault_01;
+
+public class GoodbyeWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.printf("Goodbye, World!");
+	}
+
+}
